fbd83bc060d0a445556cc8647bb06b379bddd5b1
xieyang <xieyang@memect.co>
2018-03-07 17:58:46 +0800

    增加oauth redirect

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Notifications from 'vue-notification'
+import VueRouter from 'vue-router'
+
+import Redirect from './components/Redirect'
+import LoginAndRegister from './components/HelloWorld'
 
 Vue.config.productionTip = false;
 
 Vue.use(Notifications)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: LoginAndRegister },
+  { path: '/redirect', component: Redirect }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
